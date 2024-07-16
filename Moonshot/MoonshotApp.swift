a97b012ae0bcd39f8baa5e335affa64fc2103edb
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-15 23:51:35 -0400

    Initial commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Rodrigo on 15-07-24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
