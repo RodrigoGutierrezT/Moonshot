b0510f4980b9e158034ce2b00105efd742a57265
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-16 17:38:00 -0400

    feat: Add Mission and CrewRole struct

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,22 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Rodrigo on 16-07-24.
+//
+
+import Foundation
+
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
