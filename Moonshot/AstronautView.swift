b20973e06723e41ea3308c924fe53300290d2cd0
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-17 19:22:48 -0400

    feat: Add AstronautView implementation

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Rodrigo on 17-07-24.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return AstronautView(astronaut: astronauts["grissom"]!)
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
