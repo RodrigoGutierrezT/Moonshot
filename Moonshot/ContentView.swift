571aecbc23bc14c613a2d84df64347918b097f85
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-16 22:34:28 -0400

    feat: Add main layout basic structure

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        VStack {
+                            NavigationLink {
+                                Text("Detail View")
+                            } label: {
+                                VStack {
+                                    Image(mission.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                                    
+                                    VStack {
+                                        Text(mission.displaName)
+                                            .font(.headline)
+                                        
+                                        Text(mission.formattedLaunchDate)
+                                            .font(.caption)
+                                    }
+                                    .frame(maxWidth: .infinity)                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
         }
-        .padding()
     }
 }
 
