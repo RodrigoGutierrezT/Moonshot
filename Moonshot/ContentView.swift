450640b5c021b9dd083527f232e6230cd8e2e14d
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-17 16:49:55 -0400

    feat: Add content view navigation to Mission view and MissionView basic layout implementation

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                     ForEach(missions) { mission in
                         VStack {
                             NavigationLink {
-                                Text("Detail View")
+                                MissionView(mission: mission, astronauts: astronauts)
                             } label: {
                                 VStack {
                                     Image(mission.image)
